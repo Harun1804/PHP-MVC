3e6b35345986952a744ed0f9ae316c80bf5ca41a
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-21 09:15:10 +0700

    Routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,18 @@
 <?php
 
 class App {
-    
+    public function __construct()
+    {
+        $url = $this->parseURL();
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'],'/');
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
+    }
 }
