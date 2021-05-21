37ba77b2a11864328f58ea9b5ed906a61757b46e
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-21 09:32:05 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About {
+
+    public function index($nama = 'nama',$pekerjaan = 'Programmer')
+    {
+        echo "Halo, Nama saya $nama, Saya seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        
+    }
+}
