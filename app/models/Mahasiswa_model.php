5f1616a327d0a474f3d6d7c391af3e12f94f3e6a
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-21 20:59:04 +0700

    insert data

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -21,4 +21,24 @@ class Mahasiswa_model extends Database {
         $this->db->bind('id',$id);
         return $this->db->single();
     }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "  INSERT INTO mahasiswa
+                    VALUES(
+                        '',
+                        :nama,
+                        :npm,
+                        :email,
+                        :jurusan
+                    )";
+        $this->db->query($query);
+        $this->db->bind('nama',$data['nama']);
+        $this->db->bind('npm',$data['npm']);
+        $this->db->bind('email',$data['email']);
+        $this->db->bind('jurusan',$data['jurusan']);
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,16 +1,61 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+            <br>
             <h3>Daftar Mahasiswa</h3>
-
             <ul class="list-group">
-                <?php foreach($data['mhs'] as $mhs) :?>
+                <?php foreach ($data['mhs'] as $mhs) : ?>
                     <li class="list-group-item">
                         <?= $mhs['nama']; ?>
                         <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary float-end">Detail</a>
                     </li>
-                <?php endforeach;?>
+                <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                        <label for="npm" class="form-label">Nomor Induk Mahasiswa</label>
+                        <input type="number" class="form-control" id="npm" name="npm">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <label for="jurusan" class="form-label">Jurusan</label>
+                    <select class="form-select" aria-label="Default select example" name="jurusan" id="jurusan">
+                        <option selected disabled>Pilih Jurusan</option>
+                        <option value="teknik informatika">Teknik Informatika</option>
+                        <option value="managemen informatika">Managemen Informatika</option>
+                        <option value="logistik bisnis">Logistik Bisnis</option>
+                        <option value="management bisnis">Management Bisnis</option>
+                        <option value="akuntansi">Akuntansi</option>
+                        <option value="ecommerce">Ecommerce</option>
+                    </select>
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
