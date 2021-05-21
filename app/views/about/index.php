d83214f5231510ff29b9df70a72aadbdf82f390b
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-21 10:29:04 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,13 @@
-    <h1>About Me</h1>
-    <p>
-        Halo, Nama saya <?= $data['nama'] ;?>, Umur saya <?= $data['umur']?> , Memiliki Pekerjaan sebagai seorang <?= $data['pekerjaan']?>
-    </p>
+<div class="container py-4">
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <img src="<?= BASEURL.'/img/kiva.jpg' ?>" alt="Profile" width="50">
+            <h1 class="display-5 fw-bold">About Me</h1>
+            <p class="col-md-8 fs-4">
+                Halo, Nama saya <?= $data['nama'] ;?>, <br>
+                Umur saya <?= $data['umur']?> , <br>
+                Memiliki Pekerjaan sebagai seorang <?= $data['pekerjaan']?>
+            </p>
+        </div>
+    </div>
+</div>
